b7f3f94bf3b52680f81cd53eca2d7c7483be74e2
teakuk1003 <teakuk1003@gmail.com>
2021-03-13 19:30:49 +0900

    1

---
+++ chap06/OverrideTest.java
@@ -0,0 +1,46 @@
+package chap06;
+
+class Vehicle {
+	String color;
+	int speed;
+
+	public Vehicle(String color, int speed) {
+		super();
+		this.color = color;
+		this.speed = speed;
+	}
+
+	void show() {
+		System.out.println("색상 : " + color);
+		System.out.println("속도 : " + speed);
+	}
+}
+
+class Car extends Vehicle {
+	int displacement;
+	int gears;
+
+	public Car(String color, int speed, int displacement, int gears) {
+		super(color, speed);
+		this.displacement = displacement;
+		this.gears = gears;
+	}
+
+	void show() {
+		System.out.println("자동차 색상 : " + color);
+		System.out.println("자동차 속도 : " + speed);
+		System.out.println("자동차 배기량 : " + displacement);
+		System.out.println("자동차 기어 단수 : " + gears);
+	}
+}
+
+public class OverrideTest {
+	public static void main(String[] args) {
+		Car c = new Car("파랑", 200, 1000, 5);
+		c.show();
+
+		System.out.println();
+		Vehicle v = c;
+		v.show();
+	}
+}
